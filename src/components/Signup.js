6e5f23fdb51bf4ea19e159ab4fe7ec0f3db9618c
Michael Seitz <michael.s.seitz@gmail.com>
2022-02-09 19:17:38 +0100

    working signup with redirect to login

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,20 +1,49 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+const axios = require("axios");
+const api = axios.create({ baseURL: "http://localhost:5000/api" });
 
 function Signup() {
+  const [badrequest, setBadrequest] = React.useState(false);
+  const navigator = useNavigate();
+
+  async function signup(userData) {
+    try {
+      // console.log("bla");
+      const { status } = await api.post("/signup", userData);
+      if (status === 200) {
+        navigator("/login");
+      }
+    } catch (error) {
+      setBadrequest(true);
+    }
+  }
   return (
     <div>
       <p>Please sign up here</p>
-      <form>
-        <label>
+      {badrequest && (
+        <p>User already in use or missing Username and Password</p>
+      )}
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const userData = {
+            name: event.target.name.value,
+            password: event.target.password.value,
+          };
+          signup(userData);
+        }}
+      >
+        <label htmlFor="name">
           Username
-          <input type="text"></input>
+          <input type="text" name="name"></input>
         </label>
-        <br/>
-        <label>
+        <br />
+        <label htmlFor="password">
           Password
-          <input type="text"></input>
+          <input type="password" name="password"></input>
         </label>
-        <br/>
+        <br />
         <button type="submit">Sign up</button>
       </form>
     </div>
